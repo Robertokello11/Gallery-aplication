6c11c03ee01ec521f42d96da71d7881512636e3b
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-07-07 10:17:50 +0300

    add installed apss to setting.py

--- pictureshare/settings.py
+++ pictureshare/settings.py
@@ -21,6 +21,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
